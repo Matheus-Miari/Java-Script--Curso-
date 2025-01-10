002ecdb89ae68ce781f07293980e89074db5edf2
Matheus Miari <matmiari.eng@gmail.com>
2025-01-10 12:20:46 -0300

    +

--- CONCEITOS INICIAIS/string.js
+++ CONCEITOS INICIAIS/string.js
@@ -3,3 +3,9 @@ const escola = 'Coder'
 console.log(escola.charAt(2))
 
 console.log(escola.charCodeAt(3))
+
+console.log(escola.indexOf('d'))
+
+console.log(escola.substring(1))
+
+console.log(escola.substring(0, 3))
