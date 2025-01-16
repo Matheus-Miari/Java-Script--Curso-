068480c7c8b5cb269a94974dbe0ab6a019402d5f
Matheus Miari <matmiari.eng@gmail.com>
2025-01-16 13:08:11 -0300

    +

--- CONCEITOS INICIAIS/null.js
+++ CONCEITOS INICIAIS/null.js
@@ -2,4 +2,21 @@ let valor
 
 console.log(valor)
 
-valor = null
+valor = null // ausencia de valor
+
+console.log(valor)
+
+const produto = {}
+console.log(produto.preco)
+console.log(produto)
+
+produto.preco = 3.50
+console.log(produto)
+
+produto.preco = undefined //evitar atribuir undefined
+console.log(!!produto.preco)
+console.log(produto)
+
+produto.preco = null // sem preco
+console.log(!!produto.preco)
+console.log(produto)
